5c7d50ec4ced19a4475bd9497f70a821d71cf251
yuxia <yuxia@qq.com>
2024-02-25 21:19:36 +0800

    second commit

--- src/main/java/com/yuxia/git/GitTest.java
+++ src/main/java/com/yuxia/git/GitTest.java
@@ -7,5 +7,6 @@ package com.yuxia.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
